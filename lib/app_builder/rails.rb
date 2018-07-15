351ca9fed4f1017ac46fe38dac4e4fd185422fda
i2bskn <i2bskn@gmail.com>
2018-07-16 03:00:06 +0900

    fix gemspec and add railtie

--- lib/app_builder/rails.rb
+++ lib/app_builder/rails.rb
@@ -1,7 +1,9 @@
+require "app_builder"
+
+require "app_builder/rails/railtie"
 require "app_builder/rails/version"
 
 module AppBuilder
   module Rails
-    # Your code goes here...
   end
 end
---
+++ lib/app_builder/rails/railtie.rb
@@ -0,0 +1,4 @@
+module AppBuilder::Rails
+  class Railtie < ::Rails::Railtie
+  end
+end
